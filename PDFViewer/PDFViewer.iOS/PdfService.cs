dbecb88ed719928a62f3336e9f2300748da037be
Dinis Vieira <dinisvieira@outlook.com>
2022-11-09 11:07:15 +0000

    iOS can now scale up and down

--- PDFViewer/PDFViewer.iOS/PdfService.cs
+++ PDFViewer/PDFViewer.iOS/PdfService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Security.Policy;
 using CoreGraphics;
 using Foundation;
 using UIKit;
@@ -51,8 +52,8 @@ namespace PDFViewer.iOS
                     CGContext context = UIGraphics.GetCurrentContext();
                     context.SetFillColor(1.0f, 1.0f, 1.0f, 1.0f);
 
-                    var width = pdfPage.GetBoxRect(CGPDFBox.Media).Width * resolutionMultiplier;
-                    var height = pdfPage.GetBoxRect(CGPDFBox.Media).Height * resolutionMultiplier;
+                    var width = pdfPage.GetBoxRect(CGPDFBox.Media).Width;
+                    var height = pdfPage.GetBoxRect(CGPDFBox.Media).Height;
                     System.Diagnostics.Debug.WriteLine($"Page Size: {width}x{height}");
 
                     // Gets page's bounds.
@@ -125,24 +126,39 @@ namespace PDFViewer.iOS
                     //Get PDF's page and convert as image.
                     using (CGPDFPage pdfPage = m_pdfDocument.GetPage(i))
                     {
+                        var width = pdfPage.GetBoxRect(CGPDFBox.Media).Width;
+                        var height = pdfPage.GetBoxRect(CGPDFBox.Media).Height;
+                        var targetWidth = width * resolutionMultiplier;
+                        var targetHeight = height * resolutionMultiplier;
+                        var targetCgSize = new CGSize(targetWidth, targetHeight);
+                        var targetRect = new CGRect(0, 0, targetWidth, targetHeight);
+                        var origCGSize = pdfPage.GetBoxRect(CGPDFBox.Media).Size;
+
                         //initialise image context.
-                        UIGraphics.BeginImageContext(pdfPage.GetBoxRect(CGPDFBox.Media).Size);
+                        UIGraphics.BeginImageContext(targetCgSize);
                         // get current context.
                         CGContext context = UIGraphics.GetCurrentContext();
                         context.SetFillColor(1.0f, 1.0f, 1.0f, 1.0f);
 
-                        var width = pdfPage.GetBoxRect(CGPDFBox.Media).Width * resolutionMultiplier;
-                        var height = pdfPage.GetBoxRect(CGPDFBox.Media).Height * resolutionMultiplier;
-                        System.Diagnostics.Debug.WriteLine($"Page Size: {width}x{height}");
+                        System.Diagnostics.Debug.WriteLine($"Page Size: {targetWidth}x{targetHeight}");
 
                         // Gets page's bounds.
                         CGRect bounds = new CGRect(pdfPage.GetBoxRect(CGPDFBox.Media).X, pdfPage.GetBoxRect(CGPDFBox.Media).Y, width, height);
                         if (pdfPage != null)
                         {
-                            context.FillRect(bounds);
-                            context.TranslateCTM(0, bounds.Height);
+                            context.FillRect(targetRect);
+                            context.TranslateCTM(0, targetRect.Height);
                             context.ScaleCTM(1.0f, -1.0f);
-                            context.ConcatCTM(pdfPage.GetDrawingTransform(CGPDFBox.Crop, bounds, 0, true));
+
+                            //Trying to scale up "manually"
+                            var xScale = targetCgSize.Width / bounds.Size.Width;
+                            var yScale = targetCgSize.Height / bounds.Size.Height;
+                            var scaleToApply = xScale < yScale ? xScale : yScale;
+                            context.ConcatCTM(CGAffineTransform.MakeScale(scaleToApply, scaleToApply));
+
+                            //Base scaling method. Doesn't scale up (above 100% - 1.0)
+                            //context.ConcatCTM(pdfPage.GetDrawingTransform(CGPDFBox.Crop, bounds, 0, true));
+
                             context.SetRenderingIntent(CGColorRenderingIntent.Default);
                             context.InterpolationQuality = CGInterpolationQuality.Default;
                             // Draw PDF page in the context.
