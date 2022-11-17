05d1333c259ea8c8f4c3bfd0694bb245d59d4430
Dinis Vieira <dinisvieira@outlook.com>
2022-11-17 10:52:41 +0000

    Improved iOS code

--- PDFViewer/PDFViewer.iOS/PdfService.cs
+++ PDFViewer/PDFViewer.iOS/PdfService.cs
@@ -16,132 +16,43 @@ namespace PDFViewer.iOS
         {
             try
             {
-                //Load stream Stream fileStream = ;
                 var path = NSBundle.MainBundle.PathForResource(fileName, null);
 
-                var fileStream = File.OpenRead(path);
-
-                MemoryStream stream = new MemoryStream();
-
-                // Create memory stream from file stream.
-                fileStream.CopyTo(stream);
-
-                // Create data provider from bytes.
-                CGPDFDocument m_pdfDocument = null;
-                CGDataProvider provider = new CGDataProvider(stream.ToArray());
-                try
+                using (var fileStream = File.OpenRead(path))
                 {
-                    //Load a PDF file.
-                    m_pdfDocument = new CGPDFDocument(provider);
-                }
-                catch (Exception e)
-                {
-                    System.Diagnostics.Debug.WriteLine(e);
-                }
-
-                UIImage pdfImage = null;
-
-                //Get PDF's page and convert as image.
-                using (CGPDFPage pdfPage = m_pdfDocument.GetPage(1))
-                {
-                    //initialise image context.
-                    UIGraphics.BeginImageContext(pdfPage.GetBoxRect(CGPDFBox.Media).Size);
-                    // get current context.
-                    CGContext context = UIGraphics.GetCurrentContext();
-                    context.SetFillColor(1.0f, 1.0f, 1.0f, 1.0f);
-
-                    var width = pdfPage.GetBoxRect(CGPDFBox.Media).Width;
-                    var height = pdfPage.GetBoxRect(CGPDFBox.Media).Height;
-                    System.Diagnostics.Debug.WriteLine($"Page Size: {width}x{height}");
-
-                    // Gets page's bounds.
-                    CGRect bounds = new CGRect(pdfPage.GetBoxRect(CGPDFBox.Media).X, pdfPage.GetBoxRect(CGPDFBox.Media).Y, width, height);
-                    if (pdfPage != null)
+                    using (var stream = new MemoryStream())
                     {
-                        context.FillRect(bounds);
-                        context.TranslateCTM(0, bounds.Height);
-                        context.ScaleCTM(1.0f, -1.0f);
-                        context.ConcatCTM(pdfPage.GetDrawingTransform(CGPDFBox.Crop, bounds, 0, true));
-                        context.SetRenderingIntent(CGColorRenderingIntent.Default);
-                        context.InterpolationQuality = CGInterpolationQuality.Default;
-                        // Draw PDF page in the context.
-                        context.DrawPDFPage(pdfPage);
-                        // Get image from current context.
-                        pdfImage = UIGraphics.GetImageFromCurrentImageContext();
-                        UIGraphics.EndImageContext();
-                    }
-                }
-
-                // Get bytes from UIImage object.
-                using (var imageData = pdfImage.AsPNG())
-                {
-                    var imageBytes = new byte[imageData.Length];
-                    System.Runtime.InteropServices.Marshal.Copy(imageData.Bytes, imageBytes, 0, Convert.ToInt32(imageData.Length));
-                    return imageBytes;
-                }
-            }
-            catch (Exception e)
-            {
-                System.Diagnostics.Debug.WriteLine(e);
-                return null;
-            }
-        }
-
-        public List<byte[]> LoadPdfImagePages(string fileName, double resolutionMultiplier = 1.0, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            try
-            {
-                var path = NSBundle.MainBundle.PathForResource(fileName, null);
-
-                var fileStream = File.OpenRead(path);
-
-                var list = new List<byte[]>();
-                MemoryStream stream = new MemoryStream();
-
-                // Create memory stream from file stream.
-                fileStream.CopyTo(stream);
-
-                // Create data provider from bytes.
-                CGPDFDocument m_pdfDocument = null;
-                CGDataProvider provider = new CGDataProvider(stream.ToArray());
-                try
-                {
-                    //Load a PDF file.
-                    m_pdfDocument = new CGPDFDocument(provider);
-                }
-                catch (Exception e)
-                {
-                    System.Diagnostics.Debug.WriteLine(e);
-                }
-
-                nint pageCount = m_pdfDocument.Pages;
-                for (int i = 1; i <= pageCount; i++)
-                {
-                    UIImage pdfImage = null;
-
-                    //Get PDF's page and convert as image.
-                    using (CGPDFPage pdfPage = m_pdfDocument.GetPage(i))
-                    {
-                        var width = pdfPage.GetBoxRect(CGPDFBox.Media).Width;
-                        var height = pdfPage.GetBoxRect(CGPDFBox.Media).Height;
-                        var targetWidth = width * resolutionMultiplier;
-                        var targetHeight = height * resolutionMultiplier;
-                        var targetCgSize = new CGSize(targetWidth, targetHeight);
-                        var targetRect = new CGRect(0, 0, targetWidth, targetHeight);
-                        var origCGSize = pdfPage.GetBoxRect(CGPDFBox.Media).Size;
-
-                        //initialise image context.
-                        UIGraphics.BeginImageContext(targetCgSize);
-                        // get current context.
-                        CGContext context = UIGraphics.GetCurrentContext();
-                        context.SetFillColor(1.0f, 1.0f, 1.0f, 1.0f);
-
-                        System.Diagnostics.Debug.WriteLine($"Page Size: {targetWidth}x{targetHeight}");
-
-                        // Gets page's bounds.
-                        CGRect bounds = new CGRect(pdfPage.GetBoxRect(CGPDFBox.Media).X, pdfPage.GetBoxRect(CGPDFBox.Media).Y, width, height);
-                        if (pdfPage != null)
+                        fileStream.CopyTo(stream);
+                        CGDataProvider provider = new CGDataProvider(stream.ToArray());
+                        var pdfDocument = new CGPDFDocument(provider);
+                        UIImage pdfImage = null;
+                        
+                        if (cancellationToken.IsCancellationRequested)
                         {
+                            pdfDocument?.Dispose();
+                            provider?.Dispose();
+                            return null;
+                        }
+
+                        using (CGPDFPage pdfPage = pdfDocument.GetPage(1))
+                        {
+                            if (pdfPage == null) { return null; }
+
+                            var width = pdfPage.GetBoxRect(CGPDFBox.Media).Width;
+                            var height = pdfPage.GetBoxRect(CGPDFBox.Media).Height;
+                            var targetWidth = width * resolutionMultiplier;
+                            var targetHeight = height * resolutionMultiplier;
+                            var targetCgSize = new CGSize(targetWidth, targetHeight);
+                            var targetRect = new CGRect(0, 0, targetWidth, targetHeight);
+
+                            UIGraphics.BeginImageContext(targetCgSize);
+                            var context = UIGraphics.GetCurrentContext();
+                            context.SetFillColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+                            System.Diagnostics.Debug.WriteLine($"Page Size: {targetWidth}x{targetHeight}");
+
+                            // Gets page's bounds.
+                            CGRect bounds = new CGRect(pdfPage.GetBoxRect(CGPDFBox.Media).X, pdfPage.GetBoxRect(CGPDFBox.Media).Y, width, height);
                             context.FillRect(targetRect);
                             context.TranslateCTM(0, targetRect.Height);
                             context.ScaleCTM(1.0f, -1.0f);
@@ -152,32 +63,133 @@ namespace PDFViewer.iOS
                             var scaleToApply = xScale < yScale ? xScale : yScale;
                             context.ConcatCTM(CGAffineTransform.MakeScale(scaleToApply, scaleToApply));
 
-                            //Base scaling method. Doesn't scale up (above 100% - 1.0)
+                            //Old base scaling method. Doesn't scale up (above 100% - 1.0)
                             //context.ConcatCTM(pdfPage.GetDrawingTransform(CGPDFBox.Crop, bounds, 0, true));
 
                             context.SetRenderingIntent(CGColorRenderingIntent.Default);
                             context.InterpolationQuality = CGInterpolationQuality.Default;
-                            // Draw PDF page in the context.
+
                             context.DrawPDFPage(pdfPage);
-                            // Get image from current context.
                             pdfImage = UIGraphics.GetImageFromCurrentImageContext();
                             UIGraphics.EndImageContext();
                         }
-                    }
 
-                    // Get bytes from UIImage object.
-                    using (var imageData = pdfImage.AsPNG())
-                    {
-                        var imageBytes = new byte[imageData.Length];
-                        System.Runtime.InteropServices.Marshal.Copy(imageData.Bytes, imageBytes, 0, Convert.ToInt32(imageData.Length));
-                        list.Add(imageBytes);
+                        if (pdfImage == null) { return null; }//If drawing image failed we assume the entire process fails and return null
+
+                        using (var imageData = pdfImage.AsPNG())
+                        {
+                            var imageBytes = new byte[imageData.Length];
+                            System.Runtime.InteropServices.Marshal.Copy(imageData.Bytes, imageBytes, 0, Convert.ToInt32(imageData.Length));
+                            
+                            pdfDocument?.Dispose();
+                            provider?.Dispose();
+
+                            return imageBytes;
+                        }
                     }
                 }
-
-                return list;
             }
-            catch(Exception e)
+            catch (Exception e)
             {
+                //TODO: Logger
+                System.Diagnostics.Debug.WriteLine(e);
+                return null;
+            }
+        }
+
+        public List<byte[]> LoadPdfImagePages(string fileName, double resolutionMultiplier = 1.0, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            try
+            {
+                var path = NSBundle.MainBundle.PathForResource(fileName, null);
+                var list = new List<byte[]>();
+                using (var fileStream = File.OpenRead(path))
+                {
+                    using (var stream = new MemoryStream())
+                    {
+                        fileStream.CopyTo(stream);
+                        CGDataProvider provider = new CGDataProvider(stream.ToArray());
+                        var pdfDocument = new CGPDFDocument(provider);
+
+                        if (cancellationToken.IsCancellationRequested)
+                        {
+                            pdfDocument?.Dispose();
+                            provider?.Dispose();
+                            return null;
+                        }
+
+                        nint pageCount = pdfDocument.Pages;
+                        for (int i = 1; i <= pageCount; i++)
+                        {
+                            UIImage pdfImage = null;
+                            using (CGPDFPage pdfPage = pdfDocument.GetPage(i))
+                            {
+                                if (pdfPage == null) { return null; } //If on pdfPage is null we assume the entire process fails and return null
+
+                                if (cancellationToken.IsCancellationRequested)
+                                {
+                                    pdfDocument?.Dispose();
+                                    provider?.Dispose();
+                                    return null;
+                                }
+
+                                var width = pdfPage.GetBoxRect(CGPDFBox.Media).Width;
+                                var height = pdfPage.GetBoxRect(CGPDFBox.Media).Height;
+                                var targetWidth = width * resolutionMultiplier;
+                                var targetHeight = height * resolutionMultiplier;
+                                var targetCgSize = new CGSize(targetWidth, targetHeight);
+                                var targetRect = new CGRect(0, 0, targetWidth, targetHeight);
+
+                                UIGraphics.BeginImageContext(targetCgSize);
+                                var context = UIGraphics.GetCurrentContext();
+                                context.SetFillColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+                                System.Diagnostics.Debug.WriteLine($"Page Size: {targetWidth}x{targetHeight}");
+
+                                // Gets page's bounds.
+                                CGRect bounds = new CGRect(pdfPage.GetBoxRect(CGPDFBox.Media).X, pdfPage.GetBoxRect(CGPDFBox.Media).Y, width, height);
+                                context.FillRect(targetRect);
+                                context.TranslateCTM(0, targetRect.Height);
+                                context.ScaleCTM(1.0f, -1.0f);
+
+                                //Trying to scale up "manually"
+                                var xScale = targetCgSize.Width / bounds.Size.Width;
+                                var yScale = targetCgSize.Height / bounds.Size.Height;
+                                var scaleToApply = xScale < yScale ? xScale : yScale;
+                                context.ConcatCTM(CGAffineTransform.MakeScale(scaleToApply, scaleToApply));
+
+                                //Old base scaling method. Doesn't scale up (above 100% - 1.0)
+                                //context.ConcatCTM(pdfPage.GetDrawingTransform(CGPDFBox.Crop, bounds, 0, true));
+
+                                context.SetRenderingIntent(CGColorRenderingIntent.Default);
+                                context.InterpolationQuality = CGInterpolationQuality.Default;
+
+                                context.DrawPDFPage(pdfPage);
+                                pdfImage = UIGraphics.GetImageFromCurrentImageContext();
+                                UIGraphics.EndImageContext();
+                            }
+
+                            if (pdfImage == null) { return null; }//If drawing image failed we assume the entire process fails and return null
+
+                            // Get bytes from UIImage object.
+                            using (var imageData = pdfImage.AsPNG())
+                            {
+                                var imageBytes = new byte[imageData.Length];
+                                System.Runtime.InteropServices.Marshal.Copy(imageData.Bytes, imageBytes, 0, Convert.ToInt32(imageData.Length));
+                                list.Add(imageBytes);
+                            }
+                        }
+
+                        pdfDocument?.Dispose();
+                        provider?.Dispose();
+
+                        return list;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO: Logger
                 System.Diagnostics.Debug.WriteLine(e);
                 return null;
             }
